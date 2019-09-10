6f46a6d0cbc21491cc167adb98ba208d84418ab4
Edgar Magaña <emagana11@student.rccd.edu>
2019-09-10 03:36:11 -0700

    Signed-off-by: Edgar Magaña <emagana11@student.rccd.edu>

---
+++ Hmwk/EdgarMaganaHelloWorld/main.cpp
@@ -0,0 +1,26 @@
+/* 
+ * File:   main.cpp
+ * Author: Edgar Magaña
+ * Created on September 10, 2019, 3:20 AM
+ * Purpose:  a "Hello World" program to insure that netbeans and github are working properly
+ */
+
+// System Libraries
+#include <iostream>
+using namespace std;
+
+// User Libraries
+
+// Global Variables
+
+// Function prototypes
+
+int main() {
+    // Declare Variables
+
+    // Prompt
+    cout << "Hello World!" ;
+    // Output    
+    return 0;
+}
+
