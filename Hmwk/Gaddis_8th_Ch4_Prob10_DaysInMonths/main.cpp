8e1da3704d69c3a1b5f33c3ab43a8242657b38d3
Edgar Magaña <emagana11@student.rccd.edu>
2019-09-11 21:57:55 -0700

    Signed-off-by: Edgar Magaña <emagana11@student.rccd.edu>

--- Hmwk/Gaddis_8th_Ch4_Prob10_DaysInMonths/main.cpp
+++ Hmwk/Gaddis_8th_Ch4_Prob10_DaysInMonths/main.cpp
@@ -2,7 +2,7 @@
  * File:   main.cpp
  * Author: Edgar Magaña
  * Created on September 11, 2019, 8:58 PM
- * Purpose:
+ * Purpose: Find the Days in a month
  */
 
 // System Libraries
@@ -17,10 +17,40 @@ using namespace std;
 
 int main() {
     // Declare Variables
-
+    short Month, Year, Days = 0 ; // Initialize variables 
+    bool LeapYr = false;          // Initialize bool to keep track of leap year
+    
     // Prompt
-
-    // Output    
+    while ( Month < 1 || Month > 12) {                                             // Ask user for number between 1 and 12
+        cout << "Please enter a Month in number form (1 - 12): " ;
+        cin  >> Month;
+    };
+    while ( Year < 1 || Year > 100000) {                                           // Ask user for number from 1 - 100,000
+    cout << "Please enter a Year in number form (2019): " ;
+    cin  >> Year;
+    };
+    
+    // Calculate
+    if (Year % 100 == 0 && Year % 400 == 0){                                        // Check if year is divisible by 100 and 400
+        LeapYr = true;
+    } else if (Year % 100 != 0 && Year % 4 == 0){                                   // Check if year is divisible by 4
+        LeapYr = true;
+    }
+    else LeapYr = false;                                                            // Set bool value to see if it is a leap year or not
+   
+    
+    if ( Month == 1 || Month == 3 || Month == 5 || Month == 7 || Month == 8 || Month == 10 || Month == 12){
+        Days = 31;
+    } else if ( Month == 4 || Month == 6 || Month == 9 || Month == 11){             // Assign days variable with the correct number of days in a month
+        Days = 30;
+    } else if ( Month == 2){
+        if (LeapYr == true){
+            Days = 29;                                                              // If leap year add one to the month of February
+        }else Days = 28;                                                            
+    }
+    
+    // Output
+    cout << endl << Days << " Days" << endl;
     return 0;
 }
 
