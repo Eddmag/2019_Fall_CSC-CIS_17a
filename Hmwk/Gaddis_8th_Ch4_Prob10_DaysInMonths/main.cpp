2879839c28e29a0d0396b622dd9158f3b605e939
Edgar Magaña <emagana11@student.rccd.edu>
2019-09-11 21:00:17 -0700

    Signed-off-by: Edgar Magaña <emagana11@student.rccd.edu>

---
+++ Hmwk/Gaddis_8th_Ch4_Prob10_DaysInMonths/main.cpp
@@ -0,0 +1,26 @@
+/* 
+ * File:   main.cpp
+ * Author: Edgar Magaña
+ * Created on September 11, 2019, 8:58 PM
+ * Purpose:
+ */
+
+// System Libraries
+#include <iostream>
+using namespace std;
+
+// User Libraries
+
+// Global Variables
+
+// Function prototypes
+
+int main() {
+    // Declare Variables
+
+    // Prompt
+
+    // Output    
+    return 0;
+}
+
