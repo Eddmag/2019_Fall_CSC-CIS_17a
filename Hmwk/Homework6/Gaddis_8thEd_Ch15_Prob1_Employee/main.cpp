148e30b6c22bdac72d2ca9b732dec03fa3904504
Eddmag <emagana11@student.rccd.edu>
2019-11-20 14:40:41 -0800

    Signed-off-by: Eddmag <emagana11@student.rccd.edu>

---
+++ Hmwk/Homework6/Gaddis_8thEd_Ch15_Prob1_Employee/main.cpp
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Edgar Magaña
+ * Created on November 20, 2019, 2:35 PM
+ * Purpose:  1. Employee and ProductionWorker Classes
+Design a class named Employee
+ The class should keep the following information in 
+ * • Employee name 
+ * • Employee number 
+ * • Hire date 
+ * Write one or more constructors and the appropriate accessor and mutator functions for the class.
+    Next, write a class named ProductionWorker
+    that is derived from the Employee class. The ProductionWorker class should have member variables 
+ *  to hold the following information: 
+ *  • Shift (an integer) 
+ *  • Hourly pay rate (a double ) The workday is divided into two shifts: day and night. 
+ *  The shift variable will hold an integer value representing the shift that the employee works. 
+ *  The day shift is shift 1, and the night shift is shift 
+ *  2. Write one or more constructors and the appropriate accessor and mutator functions for the class. 
+ *  Demonstrate the classes by writing a program that uses a ProductionWorker object. 
+
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+//classes
+class Employee{
+
+private:
+public:
+};
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
