09cbc4f5b980ebb8f778badedcfd4ab8254b25e2
Eddmag <eddymgn@gmail.com>
2019-11-22 15:36:01 -0800

    Signed-off-by: Eddmag <eddymgn@gmail.com>

---
+++ Proj/Project2/TexasHoldEm/main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.cpp
+ * Author: Edgar Magaña
+ * Created on November 22, 2019, 3:34 PM
+ * Purpose: Project to show concepts learned in second half of CSC-17A
+ * 
+ */
+// system libraries
+#include <iostream>
+
+using namespace std;
+
+// user libraries
+
+// classes
+
+int main(int argc, char** argv) {
+   
+    return 0;
+}
+
